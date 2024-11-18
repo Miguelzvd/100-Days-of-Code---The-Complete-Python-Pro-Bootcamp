b8214d50079ce88c841c55aefd62291900c255d3
miguelzvd <dilsonmiguel25@gmail.com>
2024-11-17 23:55:16 -0300

    Day 9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,43 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
+from art import logo
+
+should_continue = True
+bids = {}
+
 # TODO-4: Compare bids in dictionary
+def find_highest_bidder(bidding_dictionary):
+    best_bid = 0
+    best_bidder = ""
+
+    for bidder in bidding_dictionary:
+        if int(bidding_dictionary[bidder]) > best_bid:
+            best_bid = int(bidding_dictionary[bidder])
+            best_bidder = bidder
+
+    print(f"The winner is {best_bidder} with a bid of ${bids[best_bidder]}")
+    print(f"All the bidders: {bids}")
 
 
+print(logo)
+
+
+
+while should_continue:
+    # TODO-1: Ask the user for input
+    name = input("What's your name?: ")
+    price = input("What's your bid?: $")
+
+    # TODO-2: Save data into dictionary {name: price}
+
+    bids[name] = price
+
+    # TODO-3: Whether if new bids need to be added
+
+    new_bid = input("Are there any other bidders? Type 'yes' or  'no':\n")
+
+    should_continue = False
+
+    if new_bid == "yes":
+        should_continue = True
+        print("\n" * 100)
+
+find_highest_bidder(bids)
